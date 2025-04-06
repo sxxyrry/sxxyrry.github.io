3d76122b6b4a27a2d71ebd27d0f80d231191e5c9
sxxyrry <161218739+sxxyrry@users.noreply.github.com>
2025-04-06 08:24:39 +0800

    Update Website

--- FileSharer/Upload/Upload.js
+++ FileSharer/Upload/Upload.js
@@ -1,14 +1,11 @@
-import CreateFile from "../../GithubAboutFile/CreateFile.js";
-import CreateDir from "../../GithubAboutFile/CreateDir.js";
-import DirExists from "../../GithubAboutFile/DirExists.js";
-import ListDir from "../../GithubAboutFile/ListDir.js";
+import CreateFile from "../../GithubDatabase/CreateFile.js";
+import CreateDir from "../../GithubDatabase/CreateDir.js";
+import DirExists from "../../GithubDatabase/DirExists.js";
+import ListDir from "../../GithubDatabase/ListDir.js";
 import Encrypt_Decrypt from "../../Encrypt_Decrypt/Encrypt_Decrypt.js";
 
 
-const token = Encrypt_Decrypt.decrypt("1419404_1420545_1429673_1410276_1397725_1364636_1390879_1393161_1426250_1385174_1379469_\
-1377187_1418263_1358931_1431955_1396584_1400007_1404571_1413699_\
-1356649_1366918_1389738_1439942_1390879_1423968_1361213_1417122_1377187_1387456_\
-1365777_1397725_1376046_1381751_1441083_1358931_1389738_1358931_1403430_1415981_1438801_", "sxxyrry-23XR");
+const token = ""
 
 const ipt1 = document.getElementById("ipt1");
 const btn1 = document.getElementById("btn1");
