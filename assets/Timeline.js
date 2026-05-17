8bcd066d34bfa8f894f6e32d471a0831a167d95f
sxxyrry <sxxyrry@gmail.com>
2026-05-17 13:38:00 +0800

    Update Website

--- assets/Timeline.js
+++ assets/Timeline.js
@@ -5,7 +5,16 @@ const TimeLine = [
             'month': '12',
         },
         'event': [
-            '23XR Studio 正式成立',
+            '[23XR Studio](https://tt23xr.sxxyrry.qzz.io/) 正式成立',
+        ],
+    },
+    {
+        'time': {
+            'year': '2025',
+            'month': '5',
+        },
+        'event': [
+            '[官网](https://tt23xr.sxxyrry.qzz.io/)首次上线',
         ],
     },
     {
@@ -15,7 +24,7 @@ const TimeLine = [
         },
         'event': [
             'NLD 项目立项',
-            'TTHSD Golang 项目立项',
+            '[TTHSD Golang 项目](https://tt23xr.sxxyrry.qzz.io/Products/tthsd_golang.html)立项',
         ],
     },
     {
@@ -24,7 +33,7 @@ const TimeLine = [
             'month': '7',
         },
         'event': [
-            'TTHSD Golang 项目开发成功',
+            '[JustHTML 项目](https://justhtml.sxxyrry.qzz.io/)立项',
         ],
     },
     {
@@ -33,8 +42,17 @@ const TimeLine = [
             'month': '8',
         },
         'event': [
-            'TTHSD Next 项目立项',
-            'TTHSD Golang 项目停止开发',
+            '[GitHub 组织](https://github.com/TT23XR_Studio/)创建',
+        ],
+    },
+    {
+        'time': {
+            'year': '2025',
+            'month': '11',
+        },
+        'event': [
+            'TTHSD Golang v0.2.0 发布',
+            'TTHSD Golang GUI v0.1.0 发布',
         ],
     },
     {
@@ -49,10 +67,22 @@ const TimeLine = [
     {
         'time': {
             'year': '2026',
+            'month': '1',
+        },
+        'event': [
+            'JustHTML 项目完成',
+            'TTHSD Golang v0.4.0 发布',
+            'TTHSD Golang GUI v0.3.0 发布',
+        ],
+    },
+    {
+        'time': {
+            'year': '2026',
             'month': '2',
         },
         'event': [
-            'TTHSD Next 项目开发成功',
+            'TTHSD Golang v0.5.0 发布',
+            'TTHSD Golang GUI v0.4.0 发布',
         ],
     },
     {
@@ -61,7 +91,11 @@ const TimeLine = [
             'month': '3',
         },
         'event': [
-            '工作室正式更名为 TT23XR Studio',
+            '[工作室](https://tt23xr.sxxyrry.qzz.io/)正式更名为 TT23XR Studio',
+            '[KossJS 项目](https://tt23xr.sxxyrry.qzz.io/Products/kossjs.html)立项',
+            '[Vihaiper 项目](https://vihaiper.sxxyrry.qzz.io/)立项',
+            '[TTHSD Next 项目](https://tt23xr.sxxyrry.qzz.io/Products/tld.html)立项',
+            '[TTHSD Golang 项目](https://tt23xr.sxxyrry.qzz.io/Products/tthsd_golang.html)取消',
         ],
     },
     {
@@ -70,7 +104,21 @@ const TimeLine = [
             'month': '4',
         },
         'event': [
-            'TTHSD Next 项目更名为 TLD 项目',
+            '[TTHSD Next 项目](https://tt23xr.sxxyrry.qzz.io/Products/tld.html)更名为 TaiLerDownloader 项目',
+        ],
+    },
+    {
+        'time': {
+            'year': '2026',
+            'month': '5',
+        },
+        'event': [
+            '[TTP 格式规范](https://docss.sxxyrry.qzz.io/TTP/)正式发布',
+            '[TT23XR 工作室](https://tt23xr.sxxyrry.qzz.io/)与 [Vix 语言](https://vixlang.github.io/)达成合作',
+            '[Vihaiper 项目](https://vihaiper.sxxyrry.qzz.io/)上线（内部使用）',
+            '[TaiLerDownloader 宣传片](https://www.bilibili.com/video/BV16KRYBfEbu/?share_source=copy_web&vd_source=930a4f5a7e1540dcb8611add22805e35)发布',
+            '[KossJS 宣传片](https://www.bilibili.com/video/BV1oAdGBNEkt/?share_source=copy_web&vd_source=930a4f5a7e1540dcb8611add22805e35)发布',
+            'KifeJS 项目立项',
         ],
     },
 ]
